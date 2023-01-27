b25dbdbabe583426e239d0c47562a3ec136e125b
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-27 19:11:35 +0100

    first commit

---
+++ saluto.php
@@ -0,0 +1,21 @@
+<?php
+$paragrafo = $_POST["paragrafo"];
+$censura = $_POST["censura"]
+
+str_replace()
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo $paragrafo?></h1>
+    <h1><?php echo $censura?></h1>
+</body>
+</html>
